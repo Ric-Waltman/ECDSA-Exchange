a5887d176d0ac38fa3f4c9a55a389bbfc0c2abbc
Ric Waltman <0xglitch@protonmail.com>
2022-05-20 11:26:23 -0700

    Project 1 Completion Using Digital Signatures

--- client/index.js
+++ client/index.js
@@ -1,4 +1,5 @@
 import "./index.scss";
+const secp = require('@noble/secp256k1');
 
 const server = "http://localhost:3042";
 
@@ -15,14 +16,41 @@ document.getElementById("exchange-address").addEventListener('input', ({ target:
   });
 });
 
-document.getElementById("transfer-amount").addEventListener('click', () => {
+document.getElementById("transfer-amount").addEventListener('click', async () => {
   const sender = document.getElementById("exchange-address").value;
+  const privateKey = document.getElementById("private-key").value;
   const amount = document.getElementById("send-amount").value;
   const recipient = document.getElementById("recipient").value;
 
-  const body = JSON.stringify({
-    sender, amount, recipient
-  });
+  // Ensure inputs are valid
+  if (!sender) {
+    console.error("`Your Public Key` must be valid");
+    return;
+  }
+  else if (!privateKey) {
+    console.error("`Private Key` must be valid");
+    return;
+  }
+  else if (!amount) {
+    console.error("`Send Amount` must be valid");
+    return;
+  }
+  else if (!recipient) {
+    console.error("`To: Public` Key must be valid");
+    return;
+  }
+
+  let message = { sender, amount, recipient };
+  const messageBuf = Buffer.from(JSON.stringify(message));
+
+  let messageHash = await secp.utils.sha256(messageBuf);
+  messageHash = Buffer.from(messageHash).toString('hex');
+
+  let signature = await secp.sign(messageHash, privateKey);
+  signature = Buffer.from(signature).toString('hex');
+
+  console.log({message, signature});
+  const body = JSON.stringify({message, signature});
 
   const request = new Request(`${server}/send`, { method: 'POST', body });
 
--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+const secp = require('@noble/secp256k1');
 const express = require('express');
 const app = express();
 const cors = require('cors');
@@ -8,23 +9,49 @@ const port = 3042;
 app.use(cors());
 app.use(express.json());
 
-const balances = {
-  "1": 100,
-  "2": 50,
-  "3": 75,
-}
+// Maps Public Keys to Amounts
+let accounts = {
+  "0x508e2ce37cb80296005a1bfb9b23a958f0c0376a": {
+    amount: 100,
+    fullPublicKey: "04049270e90ca01b390c4c48fca482b212a1e3140b4768048af8ff6096005775ad9ce1abfebf25802246646f68508e2ce37cb80296005a1bfb9b23a958f0c0376a"
+  },
+  "0xde8a37901b2ff4aa0de460b725e1ac660ea51175": {
+    amount: 100,
+    fullPublicKey: "04f31f962f8fa6100efe47a8db0d88dbfb617b8cc1d77b153c43e4f42078ca20e0f44a947c8b9dc455aa7300fcde8a37901b2ff4aa0de460b725e1ac660ea51175"
+  },
+  "0x4d5852ffb0976e0d7bb62d537753d5da412c222e": {
+    amount: 100,
+    fullPublicKey: "04c96abec62cf74061bc31bff2bc8695badd0d5c2db0a132e6b005ea09683d8b0ee8409e31fd40458962e47faf4d5852ffb0976e0d7bb62d537753d5da412c222e"
+  },
+};
+
+console.log(accounts);
 
 app.get('/balance/:address', (req, res) => {
   const {address} = req.params;
-  const balance = balances[address] || 0;
+  const balance = accounts[address].amount || 0;
   res.send({ balance });
 });
 
-app.post('/send', (req, res) => {
-  const {sender, recipient, amount} = req.body;
-  balances[sender] -= amount;
-  balances[recipient] = (balances[recipient] || 0) + +amount;
-  res.send({ balance: balances[sender] });
+app.post('/send', async (req, res) => {
+  console.log(req.body);
+
+  const {message, signature} = req.body;
+  const {sender, amount, recipient} = message;
+
+  // Verify Signature
+  const messageBuf = Buffer.from(JSON.stringify(message));
+  let messageHash = await secp.utils.sha256(messageBuf);
+  const isValid = secp.verify(signature, messageHash, accounts[sender].fullPublicKey);
+
+  console.log("Signature isValid=" + isValid);
+  if(isValid) {
+    accounts[sender].amount -= amount;
+    accounts[recipient].amount = (accounts[recipient].amount || 0) + +amount;
+    res.send({ balance: accounts[sender].amount });
+  } else {
+    console.error("Signature is invalid!");
+  }
 });
 
 app.listen(port, () => {
